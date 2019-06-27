404280d79670c87486e0fb36073d12d02524aa1d
Becheru888 <becheru93@gmail.com>
2019-06-27 14:46:53 +0100

    route added to app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Route, Redirect } from "react-router";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Route
+          path="/"
+          render={pr =>
+            localStorage.getItem("token") ? (
+              <div>
+                <AddFriend {...pr} />
+                <Friends {...pr} />
+              </div>
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
+        <Route path="/login" render={props => <Login {...props} />} />
+      </div>
+    );
+  }
 }
 
 export default App;
